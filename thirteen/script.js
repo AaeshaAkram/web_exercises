f9c35a1d37fcea8d69acc34f1fd6fb752a90a491
AaeshaAkram <76445586+AaeshaAkram@users.noreply.github.com>
2024-06-30 13:35:32 +0400

    Add files via upload

---
+++ thirteen/script.js
@@ -0,0 +1,24 @@
+//JS DATA STRUCTURES
+//2: OBJECT
+
+var user={
+	name:"John",
+	age:34,
+	hobby:"Basketball",
+	isMarried: false,
+	spells: ["boo", "abra", "ouu"],
+	shout:function(){
+		console.log("AHHH!");
+	}
+};
+
+var list=[
+	{
+		username:"Andy",
+		Pw:"123"
+	},
+	{
+		username:"yuta",
+		Pw:"987"
+	}
+];
