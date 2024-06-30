9410fd02c842739b9c02b34ebc4bcfbb7cd64f23
AaeshaAkram <76445586+AaeshaAkram@users.noreply.github.com>
2024-06-30 13:34:04 +0400

    Add files via upload

---
+++ eleven/script.js
@@ -0,0 +1,46 @@
+// 4+3;
+// if(4+3==7)
+// {
+// 	// alert("Correct");
+// 	console.log("Hello");
+// }
+
+// USE OF FUNCTIONS
+// 1: Function Declaration
+function sayHello()
+{
+	console.log("Hello");
+}
+
+sayHello();
+
+// --------------------
+// Using ARGUMENTS
+function sing(song)
+{
+	console.log(song);
+}
+sing("Laaa deeeee daaa");
+sing("ouuuu");
+
+// --------------------
+//2:Function Expression
+var sayBye=function()
+{
+	console.log("Bye");
+}
+
+sayBye();
+
+// --------------------
+
+function multiply(a,b)
+{
+	return a*b;
+}
+alert(multiply(4,5));
+
+
+
+
+
